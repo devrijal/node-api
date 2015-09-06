b0e59ba16905eebcd6d2e887bd5948ad70cf3c36
rijal1410 <rijal@livaza.com>
2015-09-06 14:58:41 +0700

    adding angular example to node-api

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+// name our angular app
+angular.module('firstApp', [])
+
+.controller('mainController', function(){
+
+	// bind this to vm (view model)
+	var vm = this;
+
+	// define variables and objects on this
+	// this lets them be available to our view
+
+	// define a basic variable
+	vm.message = 'Hey there! Come and see how good I look!';
+
+	// define a list of items
+	vm.computers = [
+		{ name: 'Macbook Pro', color: 'Silver', nerdness: 7 },
+		{ name: 'Yoga 2 Pro', color: 'Gray', nerdness: 6 },
+		{ name: 'Chromebook', color: 'Black', nerdness: 5 }
+	];
+
+	// information  that comes from our form
+	vm.computerData = {};
+
+	vm.addComputer = function(){
+
+		// add a computer to the list
+		vm.computers.push({
+			name: vm.computerData.name,
+			color: vm.computerData.color,
+			nerdness: vm.computerData.nerdness
+		});
+
+		// after our computer has been adderd, clar the form
+		vm.computerData = {};
+	};
+});
